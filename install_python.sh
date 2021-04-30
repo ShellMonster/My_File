5a167eea5530b0c6033beeb8cb8c85074c8faffb
ShellMonster <Ctrlf4@yeah.net>
2021-04-30 11:18:59 +0800

    Update install_python.sh

--- install_python.sh
+++ install_python.sh
@@ -2,38 +2,37 @@
 # yum install gcc -y   zlib-devel openssl-devel
 ###### 安装前检查 ######
 # 快捷执行命令：
-# 
-echo 111
+# curl -o- -L https://raw.githubusercontent.com/ShellMonster/My_File/main/install_python.sh | bash -s
 
-# 切换到首页
-# cd  
+切换到首页
+cd  
 
-# # 下载3.8.0安装包；
-# wget --no-check-certificate http://npm.taobao.org/mirrors/python/3.8.0/Python-3.8.0.tgz
+# 下载3.8.0安装包；
+wget --no-check-certificate http://npm.taobao.org/mirrors/python/3.8.0/Python-3.8.0.tgz
 
-# # 解压缩3.8.0；
-# tar -zxvf Python-3.8.0.tgz
+# 解压缩3.8.0；
+tar -zxvf Python-3.8.0.tgz
 
-# # 进入解压后的目录；
-# cd Python-3.8.0
+# 进入解压后的目录；
+cd Python-3.8.0
 
-# # 开始编译版本到对应Python目录；
-# ./configure --with-ssl --prefix=/usr/local/python3.8
+# 开始编译版本到对应Python目录；
+./configure --with-ssl --prefix=/usr/local/python3.8
 
-# # 安装Python
-# make & make install
+# 安装Python
+make & make install
 
-# # 删除原有Python快捷方式(有就删掉)
-# rm -rf /usr/bin/python3
+# 删除原有Python快捷方式(有就删掉)
+rm -rf /usr/bin/python3
 
-# # 创建快捷方式；
-# ln -s /usr/local/python3.8/bin/python3.8 /usr/bin/python3
-# ln -s /usr/local/python3.8/bin/pip3 /usr/bin/pip3
+# 创建快捷方式；
+ln -s /usr/local/python3.8/bin/python3.8 /usr/bin/python3
+ln -s /usr/local/python3.8/bin/pip3 /usr/bin/pip3
 
-# # 安装完毕后查看当前版本；
-# python -V
+# 安装完毕后查看当前版本；
+python -V
 
-# # 删除下载的文件和目录；
-# cd ..
-# rm -rf Python-3.8.0.tgz
-# rm -rf Python-3.8.0
+# 删除下载的文件和目录；
+cd ..
+rm -rf Python-3.8.0.tgz
+rm -rf Python-3.8.0
